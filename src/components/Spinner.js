56e2ed7a78d5cb1d4a7fddfa705d7ec413f67b13
hbin1911 <rajas.mulik@gmail.com>
2023-08-19 15:28:24 +0530

    Added spinner loader

---
+++ src/components/Spinner.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Settings from './Settings.gif'
+
+export class Spinner extends Component {
+  render() {
+    return (
+      <div className='text-center'>
+        <img src={Settings} alt="loading" />
+      </div>
+    )
+  }
+}
+
+export default Spinner
