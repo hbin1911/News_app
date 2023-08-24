60b754a271de169775cd634492eb721d2b464fd3
hbin1911 <rajas.mulik@gmail.com>
2023-08-24 22:09:19 +0530

    Top loading bar added

--- src/components/News.js
+++ src/components/News.js
@@ -33,13 +33,17 @@ export class News extends Component {
   }
   
   async updateNews(){
+    this.props.setProgress(10);
     const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=4fa506642d994a779b08e83c65459c6b&page=${this.state.page}&pageSize=${this.props.pageSize}`
     this.setState({loading: true})
     let data = await fetch(url);
+    this.props.setProgress(30);
     let parsedData = await data.json();
+    this.props.setProgress(70);
     this.setState({articles: parsedData.articles,
        totalResults: parsedData.totalResults,
        loading :false});
+       this.props.setProgress(100);
   }
   
   async componentDidMount(){
